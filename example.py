3cb2be19f38985c5e7f261c333e9fa3128635d75
Evan Berkowitz <git@evanberkowitz.com>
2022-04-02 13:05:58 +0200

    Add an example nonstandard default that is a slice

--- example.py
+++ example.py
@@ -13,6 +13,7 @@ parser.add_argument('--default-to-nothing',    nargs='*', action=ParseSlice.Pars
 parser.add_argument('--star-star-everything', **ParseSlice.SliceEverything)
 parser.add_argument('--star-star-nothing',    **ParseSlice.SliceNothing)
 parser.add_argument('--star-star-default',    **ParseSlice.SliceWithDefault([2, 3, 5, 7]))
+parser.add_argument('--star-star-evens',      **ParseSlice.SliceWithDefault(slice(0,None,2)))
 
 args = parser.parse_args()
 
@@ -24,3 +25,4 @@ print(f"Default to    nothing: {x[args.default_to_nothing]}")
 print(f"Star star  everything: {x[args.star_star_everything]}")
 print(f"Star star     nothing: {x[args.star_star_nothing]}")
 print(f"Star star     default: {x[args.star_star_default]}")
+print(f"Star star       evens: {x[args.star_star_evens]}")
